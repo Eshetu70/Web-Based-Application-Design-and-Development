53dd044922d6351e40f17de18e0eb6b43c70cce4
eshetu70 <eshetuwek1@gmail.com>
2022-10-25 15:38:43 -0400

    updated

---
+++ itis3135/hobby/scripts/midterm.js
@@ -0,0 +1,7 @@
+
+
+window.onload = function () {
+    document.getElementById("display").onclick = displayResults;
+    document.getElementById("add_salary").onclick = addSalary;
+    document.getElementById("display_salary").onclick = displaySalary;
+};
---
+++ itis3135/scripts/midterm.js
@@ -0,0 +1,6 @@
+
+window.onload = function () {
+    document.getElementById("display").onclick = displayResults;
+    document.getElementById("add_salary").onclick = addSalary;
+    document.getElementById("display_salary").onclick = displaySalary;
+};
