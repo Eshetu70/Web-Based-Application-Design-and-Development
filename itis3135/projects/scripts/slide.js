b0829e6ae3044885e23c4a8391a4418493609eed
eshetu70 <eshetuwek1@gmail.com>
2022-11-05 21:09:26 -0400

    updated

---
+++ itis3135/projects/scripts/slide.js
@@ -0,0 +1,28 @@
+
+
+function About(evt, hobbys) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(hobbys).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+function About(evt, hobbys) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(hobbys).style.display = "block";
+    evt.currentTarget.className += " active";
+}
