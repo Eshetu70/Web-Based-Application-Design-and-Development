377d2c13261a0d69eb107cfaee8a809a2ac743c4
eshetu70 <eshetuwek1@gmail.com>
2022-11-22 10:28:32 -0500

    add

--- itis3135/projects/scripts/sliders.js
+++ itis3135/projects/scripts/sliders.js
@@ -3,12 +3,13 @@ $(document).ready(function() {
         auto: true,
         minSlides: 1,
         maxSlides: 1,
-        slideWidth: 500,
-        slideMargin: 25,
+        slideWidth: 600,
+        slideMargin: 20,
         randomStart: true,
         captions: true,
         pause:3000,
         pager:true,
         pagerType:"short"
+        
     });
 });
